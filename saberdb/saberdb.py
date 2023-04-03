0da513bcf606c313e5c6213c121b8458738ac7b7
phoxwupsh <phoxwupsh@gmail.com>
2023-04-03 19:42:35 +0800

    seperate hasher from database, and simplify some apis

--- saberdb/saberdb.py
+++ saberdb/saberdb.py
@@ -2,9 +2,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from genericpath import isfile
 from sqlite3 import connect, Error
-from imagehash import ImageHash, hex_to_hash, hex_to_flathash
-if not __name__ == '__main__':
-    from saberdb.hasher import Hasher
+from hasher import Hasher
 from utils import split_list
 from PIL import Image
 from os import stat
@@ -12,9 +10,8 @@ from threading import Thread, RLock
 from multiprocessing import cpu_count
 
 class SaberDB():
-    def __init__(self, config: SaberDBConfig, hasher: Hasher) -> None:
+    def __init__(self, config: SaberDBConfig) -> None:
         self.config = config
-        self.hasher = hasher
         self.lock = RLock()
         self.conn = None
         self.cur = None
@@ -24,27 +21,23 @@ class SaberDB():
         except Error as e:
             print(e)
         self.cur.execute('CREATE TABLE IF NOT EXISTS `saberdb` (hash text PRIMARY KEY, width integer, height integer, size integer, path text);')
-        if self.config.check_db:
-            self.check_db()
     
-    def is_img_in_db(self, img: ImageHash | str) -> bool:
-        hash_obj = self.__get_hash_obj(img)
+    def is_img_in_db(self, img_hash: str) -> bool:
         self.lock.acquire()
-        self.cur.execute(f'SELECT hash FROM `saberdb` WHERE hash="{hash_obj}"')
+        self.cur.execute(f'SELECT hash FROM `saberdb` WHERE hash="{img_hash}"')
         self.lock.release()
         return not self.cur.fetchone() is None
     
-    def is_img_valid(self, img_hash: ImageHash | str, img_path) -> bool:
-        hash_obj = self.__get_hash_obj(img_hash)
+    def is_img_valid(self, hasher: Hasher, img_hash: str, img_path: str) -> bool:
         if not isfile(img_path):
             return False
         with Image.open(img_path) as target_img:
-            target_hash = self.hasher.hash(target_img)
-            return hash_obj == target_hash
+            target_hash = hasher.hash(target_img)
+            return img_hash == str(target_hash)
     
-    def add_img(self, img_path:str):
+    def add_img(self, hasher: Hasher, img_path:str):
         with Image.open(img_path) as img:
-            img_hash = self.hasher.hash(img)
+            img_hash = hasher.hash(img)
             self.cur.execute(f'INSERT INTO `saberdb` VALUES("{str(img_hash)}", {img.width}, {img.height}, {stat(img_path).st_size}, "{img_path}")')
             try:
                 self.lock.acquire()
@@ -52,29 +45,27 @@ class SaberDB():
             finally:
                 self.lock.release()
     
-    def get_img(self, img: ImageHash | str) -> ImageEntry | None:
-        hash_obj = self.__get_hash_obj(img)
-        self.cur.execute(f'SELECT hash, width, height, size, path FROM `saberdb` WHERE hash="{str(hash_obj)}"')
+    def get_img(self, img_hash: str) -> ImageEntry | None:
+        self.cur.execute(f'SELECT hash, width, height, size, path FROM `saberdb` WHERE hash="{img_hash}"')
         result = self.cur.fetchone()
         if result is None:
             return None
-        return ImageEntry(hex_to_hash(result[0]),result[1],result[2],result[3],result[4])
+        return ImageEntry(result[0],result[1],result[2],result[3],result[4])
     
-    def del_img(self, img:ImageHash | str):
-        hash_obj = self.__get_hash_obj(img)
+    def del_img(self, img_hash: str):
         self.lock.acquire()
-        self.cur.execute(f'DELETE FROM `saberdb` WHERE hash="{str(hash_obj)}"')
+        self.cur.execute(f'DELETE FROM `saberdb` WHERE hash="{img_hash}"')
         self.conn.commit()
         self.lock.release()
     
-    def check_db(self):
+    def check_db(self, hasher: Hasher):
         threads = list[Thread]()
         
         def check_db_thread(check_list:list):
             for img in check_list:
-                img_hash = self.__get_hash_obj(img[0])
+                img_hash = img[0]
                 img_path = img[1]
-                if not self.is_img_valid(img_hash, img_path):
+                if not self.is_img_valid(hasher, img_hash, img_path):
                     self.lock.acquire()
                     self.del_img(img_hash)
                     self.lock.release()
@@ -87,15 +78,10 @@ class SaberDB():
             t.start()
         for t in threads:
             t.join()
-
-    def __get_hash_obj(self, h: ImageHash | str):
-        if isinstance(h, ImageHash):
-            return h
-        return hex_to_flathash(h, self.hasher.hash_size)
     
 @dataclass
 class ImageEntry:
-    hash: ImageHash
+    hash: str
     width: int
     height: int
     size: int
