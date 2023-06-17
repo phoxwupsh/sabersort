1f5d8dd52f7181174e0da55aca2d4f816dee9dc6
phoxwupsh <phoxwupsh@gmail.com>
2023-06-17 21:04:19 +0800

    Switch to SqlAlchemy ORM schema

---
+++ saberdb/model.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class SaberRecord(Base):
+    __tablename__ = 'saberdb'
+
+    hash = Column(String, primary_key=True)
+    author = Column(String)
+    author_id = Column(String)
+    author_link = Column(String)
+    width = Column(Integer)
+    height = Column(Integer)
+    origin_link = Column(String)
+    path = Column(String)
+    size = Column(Integer)
+
+    def __init__(self, hash: str, author: str, author_id: str, author_link: str, width: int, height: int, origin_link: str, path: str, size: int) -> None:
+        self.hash = hash
+        self.author = author
+        self.author_id = author_id
+        self.author_link = author_link
+        self.width = width
+        self.height = height
+        self.origin_link = origin_link
+        self.path = path
+        self.size = size
+
+if __name__ == '__main__':
+    pass
--- saberdb/saberdb.py
+++ saberdb/saberdb.py
@@ -1,97 +1,47 @@
 from __future__ import annotations
-from dataclasses import dataclass
 from genericpath import isfile
-from sqlite3 import connect, Error
-from hasher import Hasher
-from utils import split_list
-from PIL import Image
-from os import stat
-from threading import Thread, RLock
-from multiprocessing import cpu_count
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from saberdb.model import SaberRecord, Base
+from saber.context import SaberContext
+from imagehash import ImageHash
+import atexit
 
 class SaberDB():
     def __init__(self, config: SaberDBConfig) -> None:
         self.config = config
-        self.lock = RLock()
-        self.conn = None
-        self.cur = None
-        try:
-            self.conn = connect(self.config.db_path,check_same_thread=False)
-            self.cur = self.conn.cursor()
-        except Error as e:
-            print(e)
-        self.cur.execute('CREATE TABLE IF NOT EXISTS `saberdb` (hash text PRIMARY KEY, width integer, height integer, size integer, path text);')
+        engine = create_engine(f'sqlite:///{self.config.db_path}')
+        Base.metadata.create_all(engine)
+        session = sessionmaker(bind=engine)
+        self.db = session()
+        atexit.register(self.__cleanup)
     
-    def is_img_in_db(self, img_hash: str) -> bool:
-        self.lock.acquire()
-        self.cur.execute(f'SELECT hash FROM `saberdb` WHERE hash="{img_hash}"')
-        self.lock.release()
-        return not self.cur.fetchone() is None
+    def is_img_in_db_and_valid(self, ctx: SaberContext) -> tuple[bool, bool]:
+        target = self.get(ctx.hash)
+        if target is None:
+            return False, False
+        if not isfile(target.path):
+            return True, False
+        return True, target.hash == str(ctx.hash)
     
-    def is_img_valid(self, hasher: Hasher, img_hash: str, img_path: str) -> bool:
-        if not isfile(img_path):
-            return False
-        with Image.open(img_path) as target_img:
-            target_hash = hasher.hash(target_img)
-            return img_hash == str(target_hash)
+    def add(self, item: SaberRecord):
+        self.db.add(item)
+        self.db.commit()
     
-    def add_img(self, hasher: Hasher, img_path:str):
-        with Image.open(img_path) as img:
-            img_hash = hasher.hash(img)
-            self.cur.execute(f'INSERT INTO `saberdb` VALUES("{str(img_hash)}", {img.width}, {img.height}, {stat(img_path).st_size}, "{img_path}")')
-            try:
-                self.lock.acquire()
-                self.conn.commit()
-            finally:
-                self.lock.release()
+    def get(self, img_hash: ImageHash) -> SaberRecord | None:
+        res = self.db.query(SaberRecord).filter_by(hash=str(img_hash)).one_or_none()
+        return res
     
-    def get_img(self, img_hash: str) -> ImageEntry | None:
-        self.cur.execute(f'SELECT hash, width, height, size, path FROM `saberdb` WHERE hash="{img_hash}"')
-        result = self.cur.fetchone()
-        if result is None:
-            return None
-        return ImageEntry(result[0],result[1],result[2],result[3],result[4])
-    
-    def del_img(self, img_hash: str):
-        self.lock.acquire()
-        self.cur.execute(f'DELETE FROM `saberdb` WHERE hash="{img_hash}"')
-        self.conn.commit()
-        self.lock.release()
-    
-    def check_db(self, hasher: Hasher):
-        threads = list[Thread]()
-        
-        def check_db_thread(check_list:list):
-            for img in check_list:
-                img_hash = img[0]
-                img_path = img[1]
-                if not self.is_img_valid(hasher, img_hash, img_path):
-                    self.lock.acquire()
-                    self.del_img(img_hash)
-                    self.lock.release()
+    def delete(self, img_hash: ImageHash):
+        self.db.query(SaberRecord).filter_by(hash=str(img_hash)).delete()
+        self.db.commit()
 
-        self.cur.execute('SELECT hash, path FROM `saberdb`')
-        check_list = self.cur.fetchall()
-        for l in split_list(check_list, self.config.threads):
-            t = Thread(target=check_db_thread, args=(l,))
-            threads.append(t)
-            t.start()
-        for t in threads:
-            t.join()
-    
-@dataclass
-class ImageEntry:
-    hash: str
-    width: int
-    height: int
-    size: int
-    path: str
+    def __cleanup(self):
+        self.db.close_all()
 
 class SaberDBConfig:
-    def __init__(self, db_path: str = 'saberdb.db', check_db:bool=False, threads: int=None) -> None:
+    def __init__(self, db_path: str='saberdb.db') -> None:
         self.db_path = db_path
-        self.check_db = check_db
-        self.threads = cpu_count() if threads is None or threads < 1 else threads
 
 if __name__ == "__main__":
     pass
